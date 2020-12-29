aaff050e9a14087338a4b7c8b8b5fe4e64b619fa
FahmiaNuhaShafira <71803376+FahmiaNuhaShafira@users.noreply.github.com>
2020-12-29 16:37:36 +0700

    Update chapter 10 - praktikum 3.py

--- chapter 10 - praktikum 3.py
+++ chapter 10 - praktikum 3.py
@@ -8,6 +8,6 @@ listDict=[]
 for i in range(len(baris)) :
     a=baris[i].split('|')
     a[2]=a[2].replace('\n', '')
-    dicti={'nama' : a[0], 'nim' : a[1], 'alamat' : a[2]}
+    dicti={'nim' : a[0], 'nama' : a[1], 'alamat' : a[2]}
     listDict.append(dicti)
 print(listDict)
